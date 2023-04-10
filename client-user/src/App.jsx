9ff2a4b4c5d1d6cc3578908c318d6936d134981d
Satrio Prabowo <satrioprabowo47@gmail.com>
2023-04-10 16:38:23 +0700

    chore: add Navbar and Homepage

---
+++ client-user/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react" 
+import Navbar from "./components/Navbar.jsx"
+import Homepage from "./pages/Home.jsx"
+
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Homepage />
+    </>
+  )
+}
+
+export default App
