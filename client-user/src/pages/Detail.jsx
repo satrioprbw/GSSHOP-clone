2b986c35daeee4c11fafc33a3c115efc17e057c8
satrioprbw <satrioprabowo47@gmail.com>
2023-04-11 23:56:37 +0700

    feat: show images in detail

--- client-user/src/pages/Detail.jsx
+++ client-user/src/pages/Detail.jsx
@@ -3,7 +3,7 @@ import useFetch from "../hooks/useFetch"
 import { useParams } from "react-router-dom"
 
 function ProductDetail() {
-  let { data } = useFetch("http://localhost:3000/products?_expand=platform&_expand=author")
+  let { data } = useFetch("http://localhost:3000/products?_expand=platform&_embed=images")
   const { slug } = useParams()
   data = data.filter(el => el.slug === slug)
   console.log(data);
@@ -11,9 +11,28 @@ function ProductDetail() {
     <>
       <div>
         <div className="grid grid-cols-2 md:grid-cols-2 gap-2 ps-10 pt-10">
-          <div>
-            <img className="h-auto content-center max-w-full rounded-lg" src={data[0]?.mainImg} alt="" />
+
+          <div class="grid gap-4">
+            {data[0]?.images?.map((el, i) => {
+              if (i === 0) {
+                return (
+                  <div>
+                    <img class="h-auto max-w-full rounded-lg" src={el?.imgUrl} alt="" />
+                  </div>
+                )
+              } else {
+                return (
+                  <div class="grid grid-cols-5 gap-4">
+                    <div>
+                      <img class="h-auto max-w-full rounded-lg" src={el?.imgUrl} alt="" />
+                    </div>
+                  </div>
+                )
+
+              }
+            })}
           </div>
+
           <div className="flex flex-col ps-10">
             <h1 className="text-3xl font-bold">{data[0]?.name}</h1>
             <p>Genre        : {data[0]?.genre}</p>
@@ -24,9 +43,7 @@ function ProductDetail() {
         </div>
         <div className="ps-10 pt-10 pb-10">
           <h1 className="text-3xl py-5">Sinopsis</h1>
-          <p className="py-3">Survival is just the beginning</p>
-          <p className="py-3">Resident Evil 4 is a remake of the 2005 original Resident Evil 4. Reimagined for 2023 to bring state-of-the-art survival horror.</p>
-          <p className="py-3">Resident Evil 4 preserves the essence of the original game while introducing modernized gameplay, a reimagined storyline, and vividly detailed graphics to make this the latest survival horror game where life and death, terror, and catharsis intersect.</p>
+          <p className="py-3">{data[0]?.description}</p>
         </div>
       </div>
     </>
