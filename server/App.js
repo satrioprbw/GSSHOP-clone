05efa71a6bfcfde3c97fd8c1623f0d8c257196c1
Ubuntu <ubuntu@ip-172-31-11-131.ap-southeast-2.compute.internal>
2023-04-14 09:39:41 +0000

    from aws

--- server/App.js
+++ server/App.js
@@ -4,8 +4,6 @@ const cors = require('cors')
 const authentication = require('./middlewares/authentication')
 const app = express()
 const port = process.env.PORT || 3000
-if (process.env.NODE_ENV !== 'production') { 
-  require('dotenv').config(); }
 
 app.use(cors())
 app.use(express.json())
